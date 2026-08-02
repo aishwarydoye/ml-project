61878a5f5c9f3f560a3ee1f1c20f6884dc9064ac
Aishwary Doye <aishwarydoye333@gmail.com>
2026-08-03 00:27:42 +0530

    Final

---
+++ app.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
+
+
+@app.get("/predictdata", response_class=HTMLResponse)
+async def predict_data_get(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={}
+    )
+
+
+@app.post("/predictdata", response_class=HTMLResponse)
+async def predict_data_post(
+    request: Request,
+    gender: str = Form(...),
+    ethnicity: str = Form(...),
+    parental_level_of_education: str = Form(...),
+    lunch: str = Form(...),
+    test_preparation_course: str = Form(...),
+    reading_score: float = Form(...),
+    writing_score: float = Form(...)
+):
+
+    data = CustomData(
+        gender=gender,
+        race_ethnicity=ethnicity,
+        parental_level_of_education=parental_level_of_education,
+        lunch=lunch,
+        test_preparation_course=test_preparation_course,
+        reading_score=reading_score,
+        writing_score=writing_score
+    )
+
+    pred_df = data.get_data_as_data_frame()
+
+    print(pred_df)
+    print("Before Prediction")
+
+    predict_pipeline = PredictPipeline()
+
+    print("Mid Prediction")
+
+    results = predict_pipeline.predict(pred_df)
+
+    print("After Prediction")
+
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={
+            "results": results[0]
+        }
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
